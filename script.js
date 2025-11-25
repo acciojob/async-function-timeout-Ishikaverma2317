a224bcf3bdc042f51d71b0d97bfa0d35e2703675
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-25 14:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+btn.addEventListener("click", async () => {
+  const message = textInput.value;
+  const delay = Number(delayInput.value);
+
+  await wait(delay);
+
+  output.innerText = message;
+});
